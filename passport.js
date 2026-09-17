575295007fa39d1fa327f9f26e2e766633b34afc
SARVESH M <sarveshsarveshsaru@gmail.com>
2026-09-17 15:51:36 +0530

    Add files via upload
    Configuration

---
+++ passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID:     process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL:  process.env.GOOGLE_CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+  // Pass the raw Google profile to the callback route
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
